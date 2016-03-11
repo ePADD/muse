85dfae1e738af785f34ff7d9ae99560d35a935c3
Sudheendra Hangal <hangal@cs.stanford.edu>
2016-03-11 23:13:50 +0530

    re-added memory.css

--- WebContent/memorystudy/dumpStats.jsp
+++ WebContent/memorystudy/dumpStats.jsp
@@ -1,6 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<%@page language="java" import="java.util.*"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@page language="java" import="edu.stanford.muse.index.Archive"%>
 <%@page language="java" import="edu.stanford.muse.index.Indexer"%>
 <%@page language="java" import="edu.stanford.muse.memory.*"%>
--- src/java/edu/stanford/muse/util/Version.java
+++ src/java/edu/stanford/muse/util/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.muse.util;
 public class Version {  public static final String version = "v1.0beta"; 
 public static String appName = "muse";
-public static String buildInfo = "Built by hangal on SHs-MacBook-Pro.local, Fri 11 Mar 2016 5:06 PM git branch: ner latest commit id: 3a63d37afcd6a855d880d59a51b0b3f06fcdb026 working dir: /Users/hangal/workspace/muse";} 
+public static String buildInfo = "Built by hangal on SHs-MacBook-Pro.local, Fri 11 Mar 2016 8:21 PM git branch: ner latest commit id: a9ff23afa2b1995b65fd4b7444d56aeff0829613 working dir: /Users/hangal/workspace/muse";} 
