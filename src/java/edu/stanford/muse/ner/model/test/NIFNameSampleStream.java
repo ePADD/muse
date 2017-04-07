669e65d4f56137e959d6e1ab29162bd449fed062
vihari <viharipiratla@gmail.com>
2017-04-07 18:26:15 +0530

    Added NIF parser

--- src/java/edu/stanford/muse/ner/model/test/NIFNameSampleStream.java
+++ src/java/edu/stanford/muse/ner/model/test/NIFNameSampleStream.java
@@ -1,235 +1,116 @@
+/**
+ * This file is part of General Entity Annotator Benchmark.
+ *
+ * General Entity Annotator Benchmark is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * General Entity Annotator Benchmark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with General Entity Annotator Benchmark.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package edu.stanford.muse.ner.model.test;
 
 import edu.stanford.muse.Config;
-import edu.stanford.muse.ner.model.NEType;
-import edu.stanford.muse.util.EmailUtils;
-import edu.stanford.muse.util.Pair;
-import opennlp.tools.formats.Conll03NameSampleStream;
 import opennlp.tools.namefind.NameSample;
 import opennlp.tools.util.ObjectStream;
-import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.Span;
-import org.apache.commons.lang.StringUtils;
+import org.aksw.gerbil.dataset.Dataset;
+import org.aksw.gerbil.dataset.DatasetConfiguration;
+import org.aksw.gerbil.dataset.impl.nif.NIFFileDatasetConfig;
+import org.aksw.gerbil.datatypes.ExperimentType;
+import org.aksw.gerbil.exceptions.GerbilException;
+import org.aksw.gerbil.transfer.nif.Document;
+import org.aksw.gerbil.transfer.nif.Marking;
+import org.aksw.gerbil.transfer.nif.data.TypedNamedEntity;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
-/**
- * Created by vihari on 07/04/17.
- *
- * Parses NLP Interchange Format and emits spans
- * An example of a file in this sample can be found at: https://github.com/AKSW/n3-collection/blob/master/News-100.ttl
- */
+
 public class NIFNameSampleStream implements ObjectStream<NameSample> {
-    private final ObjectStream<String> lineStream;
 
-    int ni = 0;
-    List<NameSample> nameSamples = new ArrayList<>();
-    public NIFNameSampleStream(InputStream in) {
+    static String DUL_PERSON = "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#Person",
+            DUL_LOCATION = "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#Place",
+            DUL_ORG = "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#Organization";
+
+    //private static final String TASK1_FILE = "src/test/resources/OKE_Challenge/example_data/task1.ttl";
+
+    int di = 0;
+    List<Document> documents;
+    public NIFNameSampleStream(String filePath){
+        di = 0;
+        DatasetConfiguration datasetConfig = new NIFFileDatasetConfig("OKE_Task1", filePath, false,
+                ExperimentType.A2KB, null, null);
         try {
-            this.lineStream = new PlainTextByLineStream(in, "UTF-8");
-            System.setOut(new PrintStream(System.out, true, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            // UTF-8 is available on all JVMs, will never happen
-            throw new IllegalStateException(e);
-        }
-        ni = 0;
-
-        try {
-            collectAll();
-        } catch(IOException ie){
-            System.err.println("Could not initialize namesamples from input stream");
-            ie.printStackTrace();
-        }
-    }
-
-    private void collectAll() throws IOException{
-        Map<String, NIF> nifs = new LinkedHashMap<>();
-        List<String> block;
-        while ((block = readBlock())!=null && block.size()>0){
-            NIF nif = NIF.parse(block);
-            nifs.put(nif.id, nif);
-        }
-        //collect sentences and spans
-        Map<String, List<Span>> spans = new LinkedHashMap<>();
-        nifs.entrySet().stream()
-                .filter(e->{
-                    Set<NIF.Type> types = e.getValue().types;
-                    return !types.contains(NIF.Type.Context) && types.contains(NIF.Type.RFC5147String);
-                })
-                .forEach(e->{
-                    NIF nif = e.getValue();
-                    assert nif.ref!=null: "Parsing of NIFs not proper";
-                    String ct = null;
-                    if(nif.ref == null)
-                        System.err.println("This NIF block is unexpected: \n\n" + nif + "\n");
-
-                    if (nif.ref.startsWith("http://dbpedia.org/resource/")) {
-                        String rsc = nif.ref.substring("http://dbpedia.org/resource/".length()).replaceAll("_", "");
-                        String type = EmailUtils.readDBpedia().get(rsc);
-                        if (type!=null){
-                            NEType.Type nt = NEType.getCoarseType(NEType.parseDBpediaType(type));
-                            if (nt != NEType.Type.OTHER)
-                                ct = nt.getDisplayName();
-                        }
-                    }
-                    if(ct == null){
-                        System.err.println("Bad ref: " + nif.ref);
-                    }if(ct!=null) {
-                        Span span = new Span(nif.beginIndex, nif.endIndex, ct);
-                        String refId = nif.referenceContext;
-                        if(!spans.containsKey(refId))
-                            spans.put(refId, new ArrayList<>());
-                        spans.get(refId).add(span);
-                    }
-                });
-
-        nifs.entrySet().stream()
-                .filter(e->{
-                    Set<NIF.Type> types = e.getValue().types;
-                    return types.contains(NIF.Type.Context) && types.contains(NIF.Type.RFC5147String);
-                })
-                .forEach(e->{
-                    assert e.getValue().text!=null: "Parsing of NIFs not proper";
-                    if(e.getValue().text == null)
-                        System.err.println("This NIF block is unexpected: \n\n" + e.getValue() + "\n");
-                    String text = e.getValue().text.getFirst();
-                    String[] tokens = text.split("\\W+");
-                    List<Span> thisSpans = spans.getOrDefault(e.getValue().id, new ArrayList<>());
-                    nameSamples.add(new NameSample(tokens,
-                            thisSpans.toArray(new Span[thisSpans.size()]),
-                            nameSamples.size()==0?true:false));
-                });
-
-        int numSents = nameSamples.size();
-        int numSpans = spans.values().stream().mapToInt(List::size).sum();
-        System.out.println("Found " + numSpans + " in "+numSents+" sentences");
-        System.out.println("Total number of annotations: " + nifs.entrySet().stream()
-                .filter(e->{
-                    Set<NIF.Type> types = e.getValue().types;
-                    return !types.contains(NIF.Type.Context) && types.contains(NIF.Type.RFC5147String);
-                }).count());
-    }
-
-    List<String> readBlock() throws IOException{
-        List<String> block = new ArrayList<>();
-        String line;
-        while((line = lineStream.read())!=null){
-            //skip these...
-            if (line.startsWith("@prefix")) {
-                while ((line = lineStream.read()) != null && !StringUtils.isEmpty(line)) ;
-                line = lineStream.read();
-                if (line == null)
-                    break;
-            }
-            if(StringUtils.isEmpty(line))
-                break;
-            block.add(line.trim());
-        }
-
-        return block;
-    }
-
-    static class NIF{
-        enum Type{
-            String, Context, RFC5147String
-        }
-        String id;
-        Set<Type> types = new LinkedHashSet<>();
-        int beginIndex, endIndex;
-        String anchorOf;
-        String referenceContext;
-        //itsrdf:taIdentRef <http://dbpedia.org/resource/Clinton,_Ontario> ;
-        String ref;
-        //text and language code
-        Pair<String, String> text;
-
-        @Override
-        public String toString(){
-            StringBuffer sb = new StringBuffer();
-            sb.append("ID: "+id+"\n");
-            sb.append("Types: "+types);
-            sb.append(" [" +beginIndex + ", " + endIndex + "]\n");
-            sb.append("Anchor text: " + anchorOf + "\n");
-            sb.append("Context: " + referenceContext + "\n");
-            sb.append("Ref: " + ref + "\n");
-            sb.append("Text: "+text);
-
-            return sb.toString();
-        }
-
-        static Pattern p = Pattern.compile("([^\\s]+)[\\s\\n]+(.+?) [;.]", Pattern.MULTILINE);
-        static NIF parse(List<String> lines){
-            NIF nif = new NIF();
-            nif.id = lines.get(0);
-
-            String all = String.join(" ", lines.subList(1, lines.size()));
-            //System.out.println("ALl: "+all);
-            Matcher m = p.matcher(all);
-            while(m.find()){
-                String prop = m.group(1), obj = m.group(2);
-                //System.out.println(prop+" -- "+obj);
-                if(prop.equals("a")) {
-                    String[] stypes = obj.split(" , ");
-                    Stream.of(stypes).forEach(st->{
-                        if(st.contains(":String"))
-                            nif.types.add(Type.String);
-                        else if(st.contains(":RFC5147String"))
-                            nif.types.add(Type.RFC5147String);
-                        else if(st.contains(":Context"))
-                            nif.types.add(Type.Context);
-                    });
-                }
-                else if(prop.contains(":beginIndex"))
-                    nif.beginIndex = Integer.parseInt(obj.split("\\^\\^")[0].replaceAll("\"", ""));
-                else if(prop.contains(":endIndex"))
-                    nif.endIndex = Integer.parseInt(obj.split("\\^\\^")[0].replaceAll("\"", ""));
-                else if(prop.contains(":anchorOf"))
-                    nif.anchorOf = obj.split("\\^\\^")[0].replaceAll("\"", "");
-                else if(prop.contains(":taIdentRef"))
-                    nif.ref = obj.replaceAll("<|>", "");
-                else if(prop.contains(":isString")) {
-                    if (obj.contains("@"))
-                        nif.text = new Pair<>(obj.replaceAll("@[a-z]+$|\"",""), obj.substring(obj.indexOf("@")+1));
-                    else
-                        nif.text = new Pair<>(obj.replaceAll("\"", ""), "en");
-                }
-                else if(prop.contains(":referenceContext"))
-                    nif.referenceContext = obj;
-            }
-            return nif;
+            Dataset dataset = datasetConfig.getDataset(ExperimentType.A2KB);
+            documents = dataset.getInstances();
+        } catch (GerbilException ge) {
+            ge.printStackTrace();
         }
     }
 
     @Override
-    public NameSample read() throws IOException {
-        if(ni<nameSamples.size())
-            return nameSamples.get(ni++);
-        return null;
+    public NameSample read(){
+        if (di>=documents.size())
+            return null;
+        Document document = documents.get(di++);
+        String text = document.getText();
+        String[] tokens = text.split("\\W+");
+
+        List<Marking> markings = document.getMarkings();
+        List<Span> spans = new ArrayList<>();
+        for (int mi = 0; mi < markings.size(); mi++) {
+            Marking marking = markings.get(mi);
+            TypedNamedEntity tne = (TypedNamedEntity) marking;
+            String ct = null;
+            if (tne.getTypes().contains(DUL_PERSON))
+                ct = "PER";
+            else if (tne.getTypes().contains(DUL_LOCATION))
+                ct = "LOC";
+            else if (tne.getTypes().contains(DUL_ORG))
+                ct = "ORG";
+
+            if (ct!=null)
+                spans.add(new Span(tne.getStartPosition(), tne.getStartPosition()+tne.getLength(), ct));
+        }
+
+        return new NameSample(tokens, spans.toArray(new Span[spans.size()]), di<=1?false:true);
     }
 
     @Override
     public void reset() throws IOException, UnsupportedOperationException {
-        lineStream.reset();
+        di = 0;
     }
 
     @Override
     public void close() throws IOException {
-        lineStream.close();
     }
 
     public static void main(String[] args){
-        InputStream is = Config.getResourceAsStream("RSS-500.ttl");
-        NIFNameSampleStream nifs = new NIFNameSampleStream(is);
-        System.out.println(String.join("\n=============\n",
-                nifs.nameSamples.stream().limit(10).map(NameSample::toString).collect(Collectors.toList())));
+        String filePath = String.join(File.separator,
+                new String[]{Config.SETTINGS_DIR, "evaluation-dataset-task1.ttl"});
+        NIFNameSampleStream nss = new NIFNameSampleStream(filePath);
+        int numSents = 0, numSpans = 0;
+        int numPer = 0, numOrg = 0, numPlace = 0;
+        NameSample ns;
+        while((ns=nss.read())!=null) {
+            numSents ++;
+            numSpans += ns.getNames().length;
+            numPer += Stream.of(ns.getNames()).filter(sp->"PER".equals(sp.getType())).count();
+            numOrg += Stream.of(ns.getNames()).filter(sp->"ORG".equals(sp.getType())).count();
+            numPlace += Stream.of(ns.getNames()).filter(sp->"LOC".equals(sp.getType())).count();
+        }
+
+        System.out.println("Found " + numSents + " sentences and " + numSpans + " spans");
+        System.out.println("Found #" + numPer + "persons #" + numOrg + " orgs #" + numPlace + " places");
     }
 }
